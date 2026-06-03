58eb254eb6bc6dbe1369e0c760e19d2634a0cdc4
Ronaldo Rossetti Dearo <ronaldord.3001@gmail.com>
2026-06-03 14:38:29 -0300

    feat: add smooth scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Footer from './components/Footer'; 
+import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Explorar from './pages/Explorar';
 import Login from "./pages/Login";
@@ -14,6 +15,7 @@ import Sobre from './pages/Sobre';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Header />
       
       <main style={{ minHeight: '80vh' }}>
